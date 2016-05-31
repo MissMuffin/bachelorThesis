cad0acd6ed742906c305c64a624754992d76e274
MissMuffin <fyyree@gmail.com>
2016-05-31 12:12:08 +0200

    first iteration for viewer activity layout finished. added couple of drawables from googles open source material icons. added material colors and adjusted color scheme of app

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/ViewerActivity.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/ViewerActivity.java
@@ -2,12 +2,35 @@ package de.muffinworks.knittingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ViewerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.viewer_grid_style);
+        setContentView(R.layout.activity_viewer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_viewer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.switch_view_style) {
+            Toast.makeText(this, "Swtich view", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
