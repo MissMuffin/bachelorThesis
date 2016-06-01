f6483d3c16e0cf0bf72b77dc033f61626f613af5
MissMuffin <fyyree@gmail.com>
2016-06-01 12:06:12 +0200

    created test font and tested it in edit text in editor activity

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/EditorActivity.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/EditorActivity.java
@@ -1,7 +1,11 @@
 package de.muffinworks.knittingapp;
 
+import android.graphics.Typeface;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        EditText t = (EditText) findViewById(R.id.test);
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/KnittingTest.ttf");
+        t.setTypeface(font);
     }
 }
