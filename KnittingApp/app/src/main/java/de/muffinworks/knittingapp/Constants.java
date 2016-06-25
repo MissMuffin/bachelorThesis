5795b427a5e448216f1d0c562abcaefdab866fdf
MissMuffin <fyyree@gmail.com>
2016-06-25 22:55:05 +0200

    added xenaki's knitting font to the project for testing

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/Constants.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/Constants.java
@@ -5,6 +5,6 @@ package de.muffinworks.knittingapp;
  */
 public final class Constants {
 
-    public static final String KNITTING_FONT_PATH = "fonts/kauri_knits.ttf";
+    public static final String KNITTING_FONT_PATH = "fonts/KSymbolsW.ttf";
     public static final String KNITTING_FONT_PATH_TEST = "fonts/KnittingTest.ttf";
 }
