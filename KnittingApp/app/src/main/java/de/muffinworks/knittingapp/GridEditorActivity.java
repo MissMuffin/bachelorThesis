a689dd240cba3a97b15d0d949116a5e9812d745e
MissMuffin <fyyree@gmail.com>
2016-07-13 19:06:15 +0200

    included keyboard layout in EditorGridActivty layout. Keyboard functionality not implmented yet.

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/GridEditorActivity.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/GridEditorActivity.java
@@ -1,40 +1,31 @@
 package de.muffinworks.knittingapp;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import de.muffinworks.knittingapp.views.GridEditorView;
 
 /**
  * Created by Bianca on 11.07.2016.
  */
 public class GridEditorActivity extends AppCompatActivity {
 
-
+    private GridEditorView mGrid;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_editor);
+        mGrid = (GridEditorView) findViewById(R.id.grid);
+    }
+
+    /**
+     * inserts the text of the clicked view (button) at the position of the cursor in the edittext
+     * @param view
+     */
+    public void onButtonClick(View view) {
+
     }
 }
