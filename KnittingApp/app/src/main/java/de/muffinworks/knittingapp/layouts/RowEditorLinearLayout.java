9363bee9efd2a09ded555c6ad535bb9ab8fda161
MissMuffin <fyyree@gmail.com>
2016-07-18 10:15:47 +0200

    added scrolling functionality from TwoDScrollView

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/layouts/RowEditorLinearLayout.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/layouts/RowEditorLinearLayout.java
@@ -2,12 +2,21 @@ package de.muffinworks.knittingapp.layouts;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 
 import net.simplyadvanced.widgets.KeyboardlessEditText2;
 
@@ -28,34 +37,61 @@ public class RowEditorLinearLayout extends LinearLayout {
     private float mScaleFactor = 1.f;
 
     //Scroll
-    private boolean mIsScrolling = false;
-    private GestureDetector mGestureDetector;
+    private boolean mIsBeingDragged = false;
+    private long mLastScroll;
+    private Scroller mScroller;
+    private PointF mLastMotion = new PointF();
+    private VelocityTracker mVelocityTracker;
+    private int mTouchSlop;
+    private int mMinimumVelocity;
 
-    private float mScrolledX;
-    private float mScrolledY;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //      initial setup
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     public RowEditorLinearLayout(Context context) {
         super(context);
+        init(context);
     }
 
     public RowEditorLinearLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
+    }
 
+    public RowEditorLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
         setOrientation(HORIZONTAL);
 
         LayoutInflater inflater =  LayoutInflater.from(context);
         inflater.inflate(R.layout.row_editor, this, true);
 
+        //// TODO: 25.06.2016 line number textview and edit text should have same font for same lineheight
         lineNumbers = (LineNumberTextView) findViewById(R.id.row_editor_line_numbers);
         editText = (KeyboardlessEditText2) findViewById(R.id.row_editor_edit_text);
 
-        //// TODO: 25.06.2016 line number textview and edit text should have same font for same lineheight
-
         mScaleDetector = new ScaleGestureDetector(context, new ScaleListener(this));
-        mGestureDetector = new GestureDetector(context, new GestureListener(this));
 
+        mScroller = new Scroller(context);
+        setFocusable(true);
+        setDescendantFocusability(FOCUS_AFTER_DESCENDANTS);
         setWillNotDraw(false);
+        final ViewConfiguration config = ViewConfiguration.get(context);
+        mTouchSlop = config.getScaledTouchSlop();
+        mMinimumVelocity = config.getScaledMinimumFlingVelocity();
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //      line numbers textview and edittext functionality
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     public void initLineNumbers() {
         lineNumbers.initLineNumbers(editText.getLineCount());
@@ -76,53 +112,311 @@ public class RowEditorLinearLayout extends LinearLayout {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         //let ScaleGestureDetector handle event
-        mScaleDetector.onTouchEvent(event);
-        mGestureDetector.onTouchEvent(event);
+//        return mScaleDetector.onTouchEvent(event);
+        if (event.getAction() == MotionEvent.ACTION_DOWN && event.getEdgeFlags() != 0) return false;
+        if (!canScroll()) return false;
+
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
+
+        final int action = event.getAction();
+        final float x = event.getX();
+        final float y = event.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                //interrupt fling
+                if (!mScroller.isFinished()) mScroller.abortAnimation();
+                mLastMotion.set(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = (int) (mLastMotion.x - x);
+                int deltaY = (int) (mLastMotion.y - y);
+                mLastMotion.set(x, y);
+
+                if (deltaX < 0) {
+                    if (getScrollX() < 0) {
+                        deltaX = 0;
+                    }
+                } else if (deltaX > 0) {
+                    final int rightEdge = getWidth() - getPaddingRight();
+                    final int availableToScroll = getChildAt(1).getRight() - getScrollX() - rightEdge;
+                    if (availableToScroll > 0) {
+                        deltaX = Math.min(availableToScroll, deltaX);
+                    } else {
+                        deltaX = 0;
+                    }
+                }
+                if (deltaY < 0) {
+                    if (getScrollY() < 0) {
+                        deltaY = 0;
+                    }
+                } else if (deltaY > 0) {
+                    final int bottomEdge = getHeight() - getPaddingBottom();
+                    final int availableToScroll = getChildAt(0).getBottom() - getScrollY() - bottomEdge;
+                    if (availableToScroll > 0) {
+                        deltaY = Math.min(availableToScroll, deltaY);
+                    } else {
+                        deltaY = 0;
+                    }
+                }
+                if (deltaY != 0 || deltaX != 0) {
+                    scrollBy(deltaX, deltaY);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                final VelocityTracker velocityTracker = mVelocityTracker;
+                velocityTracker.computeCurrentVelocity(1000);
+                int initialXVelocity = (int) velocityTracker.getXVelocity();
+                int initialYVelocity = (int) velocityTracker.getYVelocity();
+
+                if ((Math.abs(initialXVelocity) + Math.abs(initialYVelocity) > mMinimumVelocity) && getChildCount() > 0) {
+                    fling(-initialXVelocity, -initialYVelocity);
+                }
+
+                if (mVelocityTracker != null) {
+                    mVelocityTracker.recycle();
+                    mVelocityTracker = null;
+                }
+        }
         return true;
     }
 
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        canvas.save(Canvas.MATRIX_SAVE_FLAG);
-//        canvas.scale(mScaleFactor, mScaleFactor);
-        canvas.translate(mScrolledX, mScrolledY);
-        super.dispatchDraw(canvas);
-        canvas.restore();
+    private void doScroll(int deltaX, int deltaY) {
+        if (deltaX != 0 || deltaY != 0) {
+            smoothScrollBy(deltaX, deltaY);
+        }
     }
 
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent ev) {
-//        final int action = MotionEventCompat.getActionMasked(ev);
-//
-//        //case that touch gesture is complete
-//        if(action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
-//            //release scroll
-//            mIsScrolling = false;
-//            //let children handle touch event, do not intercept it
-//            return false;
-//        }
-//
-//        switch(action) {
-//            case MotionEvent.ACTION_MOVE: {
-//                if (mIsScrolling) {
-//                    //currently scrolling: intercept touch event
-//                    return true;
-//                }
-//                //if horizontal finger drag exceeded touch slop, start scroll
-////                final int xDiff = calculateDistanceX(ev);
-//
-//                //calculate touch slop using ViewConfiguration constants
-////                if (xDiff > mTouchSlop) {
-//                    //start scrolling
-//                    mIsScrolling = true;
-//                    return true;
-////                }
-////                break;
-//            }
-//        }
-//        //generally don't handle touch events, should be handled by child view
-//        return false;
-//    }
+    public final void smoothScrollBy(int dx, int dy) {
+        long duration = AnimationUtils.currentAnimationTimeMillis() - mLastScroll;
+        if (duration > 250) {
+            mScroller.startScroll(getScrollX(), getScrollY(), dx, dy);
+            // awakenScrollBars(mScroller.getDuration());
+            invalidate();
+        } else {
+            if (!mScroller.isFinished()) {
+                mScroller.abortAnimation();
+            }
+            scrollBy(dx, dy);
+        }
+        mLastScroll = AnimationUtils.currentAnimationTimeMillis();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //      scroll
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private boolean canScroll() {
+        int childCount = getChildCount();
+        if (childCount > 0) {
+            int childrenHeight = 0;
+            int childrenWidth = 0;
+            for (int i = 0; i < childCount; i++) {
+                View child = getChildAt(i);
+                childrenHeight += child.getHeight();
+                childrenWidth += child.getWidth();
+            }
+            return (getHeight() < childrenHeight + getPaddingTop() + getPaddingBottom())
+                   || (getWidth() < childrenWidth + getPaddingRight() + getPaddingLeft());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action =ev.getAction();
+        if ((action == MotionEvent.ACTION_MOVE) && mIsBeingDragged) {
+            return true;
+        }
+
+        if (!canScroll()) {
+            mIsBeingDragged = false;
+            return false;
+        }
+
+        final float x = ev.getX();
+        final float y = ev.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_MOVE:
+                final int xDiff = (int) Math.abs(x - mLastMotion.x);
+                final int yDiff = (int) Math.abs(y - mLastMotion.y);
+                if (xDiff > mTouchSlop || yDiff > mTouchSlop) {
+                    mIsBeingDragged = true;
+                }
+                break;
+            case MotionEvent.ACTION_DOWN:
+                mLastMotion.x = x;
+                mLastMotion.y = y;
+                mIsBeingDragged = !mScroller.isFinished();
+                break;
+            case MotionEvent.ACTION_UP:
+                //release drag
+                mIsBeingDragged = false;
+                break;
+        }
+        //only intercept motion events if we are dragging
+        return mIsBeingDragged;
+    }
+
+    @Override
+    protected int computeVerticalScrollRange() {
+        int count = getChildCount();
+        return count == 0 ? getHeight() : (getChildAt(0)).getBottom();
+    }
+
+    @Override
+    protected int computeHorizontalScrollRange() {
+        int count = getChildCount();
+        return count == 0 ? getWidth() : (getChildAt(1)).getRight();
+    }
+
+    @Override
+    protected void measureChild(View child, int parentWidthMeasureSpec, int parentHeightMeasureSpec) {
+        ViewGroup.LayoutParams lp = child.getLayoutParams();
+
+        int childWidthMeasureSpec;
+        int childHeightMeasureSpec;
+
+        childWidthMeasureSpec = getChildMeasureSpec(parentWidthMeasureSpec, getPaddingLeft() + getPaddingRight(),
+                lp.width);
+        childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+
+        child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+    }
+
+    @Override
+    protected void measureChildWithMargins(View child, int parentWidthMeasureSpec, int widthUsed,
+                                           int parentHeightMeasureSpec, int heightUsed) {
+        final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+        final int childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(lp.leftMargin + lp.rightMargin,
+                MeasureSpec.UNSPECIFIED);
+
+        // change
+        // final int childWidthMeasureSpec =
+        // getChildMeasureSpec(parentWidthMeasureSpec, getPaddingLeft()
+        // + getPaddingRight() + lp.leftMargin + lp.rightMargin + widthUsed,
+        // lp.width);
+
+        // Change
+        final int childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(lp.topMargin + lp.bottomMargin,
+                MeasureSpec.UNSPECIFIED);
+
+        child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            int oldX = getScrollX();
+            int oldY = getScrollY();
+            int x = mScroller.getCurrX();
+            int y = mScroller.getCurrY();
+            if (getChildCount() > 0) {
+                Rect childrenDimens = getChildrenDimensions();
+                scrollTo(clamp(x, getWidth() - getPaddingRight() - getPaddingLeft(), childrenDimens.width()),
+                        clamp(y, getHeight() - getPaddingBottom() - getPaddingTop(), childrenDimens.height()));
+            } else {
+                scrollTo(x, y);
+            }
+            if (oldX != getScrollX() || oldY != getScrollY()) {
+                onScrollChanged(getScrollX(), getScrollY(), oldX, oldY);
+            }
+
+            // Keep on drawing until the animation has finished.
+            postInvalidate();
+        }
+    }
+
+    private Rect getChildrenDimensions() {
+        int childCount = getChildCount();
+        if (childCount > 0) {
+            int width = 0;
+            int height = 0;
+            for (int i = 0; i < childCount; i++) {
+                View child = getChildAt(i);
+                width += child.getWidth();
+                height += child.getHeight();
+            }
+            return new Rect(0, 0, width, height);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        // Calling this with the present values causes it to re-clam them
+        scrollTo(getScrollX(), getScrollY());
+    }
+
+    /**
+     * Fling the scroll view
+     *
+     * @param velocityY
+     *            The initial velocity in the Y direction. Positive numbers mean
+     *            that the finger/curor is moving down the screen, which means
+     *            we want to scroll towards the top.
+     */
+    public void fling(int velocityX, int velocityY) {
+        if (getChildCount() > 0) {
+            int height = getHeight() - getPaddingBottom() - getPaddingTop();
+            int bottom = getChildAt(0).getHeight();
+            int width = getWidth() - getPaddingRight() - getPaddingLeft();
+            int right = getChildAt(1).getRight();
+
+            mScroller.fling(getScrollX(), getScrollY(), velocityX, velocityY, 0, right - width, 0, bottom - height);
+            invalidate();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * This version also clamps the scrolling to the bounds of our child.
+     */
+    @Override
+    public void scrollTo(int x, int y) {
+        // we rely on the fact the View.scrollBy calls scrollTo.
+        if (getChildCount() > 0) {
+            int childrenWidth = 0;
+            int childrenHeight = 0;
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                childrenWidth += child.getWidth();
+                childrenHeight += child.getHeight();
+            }
+
+            x = clamp(x, getWidth() - getPaddingRight() - getPaddingLeft(), childrenWidth);
+            y = clamp(y, getHeight() - getPaddingBottom() - getPaddingTop(), childrenHeight);
+            if (x != getScrollX() || y != getScrollY()) {
+                super.scrollTo(x, y);
+            }
+        }
+    }
+
+    private int clamp(int currentPos, int viewDimens, int childDimens) {
+        if (viewDimens >= childDimens || currentPos < 0) {
+            return 0;
+        }
+        if (viewDimens + currentPos > childDimens) {
+            return childDimens - viewDimens;
+        }
+        return currentPos;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //      listeners
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
 
@@ -138,24 +432,6 @@ public class RowEditorLinearLayout extends LinearLayout {
             mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 2.0f));
             layout.setScaleX(mScaleFactor);
             layout.setScaleY(mScaleFactor);
-            invalidate();
-            return true;
-        }
-    }
-
-    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        private RowEditorLinearLayout layout;
-        public GestureListener(RowEditorLinearLayout layout) {
-            this.layout = layout;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            mScrolledX = distanceX;
-            mScrolledY = distanceY;
-//            layout.setScrollX((int) (layout.getScrollX() + distanceX));
-//            layout.setScrollY((int) (layout.getScrollY() + distanceY));
             invalidate();
             return true;
         }
