ee3c74691bc1cc40106978628cc9409f3c42c504
MissMuffin <fyyree@gmail.com>
2016-07-19 22:58:12 +0200

    implemented row counter and reset. added menu items for reset row counter and switch

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/ViewerActivity.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/ViewerActivity.java
@@ -4,14 +4,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ViewerActivity extends AppCompatActivity {
+
+
+    private ImageButton mIncreaseRow;
+    private ImageButton mDecreaseRow;
+    private int mRows = 0;
+    private TextView mRowText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewer);
+
+        mRowText = (TextView) findViewById(R.id.row);
+        updateRowText();
+
+        mIncreaseRow = (ImageButton) findViewById(R.id.button_increase);
+        mIncreaseRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRowText(mRows+1);
+            }
+        });
+
+        mDecreaseRow = (ImageButton) findViewById(R.id.button_decrease);
+        mDecreaseRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRowText(mRows-1);
+            }
+        });
+    }
+
+    private void updateRowText(int rows) {
+        mRows = rows;
+        mRowText.setText(Integer.toString(mRows));
+    }
+
+    private void updateRowText() {
+        updateRowText(mRows);
     }
 
     @Override
@@ -23,11 +61,12 @@ public class ViewerActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        if (id == R.id.reset_row_counter) {
+            Toast.makeText(this, "Reset counter", Toast.LENGTH_SHORT).show();
+            updateRowText(0);
             return true;
         } else if (id == R.id.switch_view_style) {
-            Toast.makeText(this, "Swtich view", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Switch view", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
