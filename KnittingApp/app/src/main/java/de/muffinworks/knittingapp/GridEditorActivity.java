feaae2314ba06d5c080d77a27a14738a86afed94
MissMuffin <fyyree@gmail.com>
2016-07-21 13:00:28 +0200

    added set grid size dialog

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/GridEditorActivity.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/GridEditorActivity.java
@@ -1,12 +1,22 @@
 package de.muffinworks.knittingapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import de.muffinworks.knittingapp.adapters.KeyboardGridAdapter;
 import de.muffinworks.knittingapp.interfaces.GridEditorKeyListener;
@@ -40,11 +50,16 @@ public class GridEditorActivity extends AppCompatActivity implements GridEditorK
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_grid_editor, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.set_size:
-            //open dialog
-                // TODO: 20.07.2016 implement 
+                showSetGridSizeDialog();
                 break;
         }
         return super.onOptionsItemSelected(item);
@@ -76,5 +91,45 @@ public class GridEditorActivity extends AppCompatActivity implements GridEditorK
         } else {
             mDeleteButtonContainer.setBackgroundColor(getResources().getColor(R.color.colorPrimary, null));
         }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //
+    //      dialog
+    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void showSetGridSizeDialog() {
+
+        LinearLayout content = (LinearLayout) getLayoutInflater().inflate(R.layout.dialog_set_grid_size, null);
+        final int columnCount = mGridEditor.getColumns();
+        final int rowCount = mGridEditor.getRows();
+
+        final EditText columns = (EditText) content.findViewById(R.id.edittext_columns);
+        columns.setText(Integer.toString(columnCount));
+
+        final EditText rows = (EditText) content.findViewById(R.id.edittext_rows);
+        rows.setText(Integer.toString(rowCount));
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        int newColumns = Integer.parseInt(columns.getText().toString());
+                        int newRows = Integer.parseInt(rows.getText().toString());
+                        if (newColumns != columnCount && newRows != rowCount) {
+                            mGridEditor.setChartSize(newRows, newColumns);
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                })
+                .setTitle("Strickmustergröße ändern")
+                .setView(content)
+                .create();
+
+        dialog.show();
     }
 }
