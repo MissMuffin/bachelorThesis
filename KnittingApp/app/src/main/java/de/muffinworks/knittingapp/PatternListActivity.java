f0779308dc4aa85977d732bf94ad1f78938b128b
MissMuffin <fyyree@gmail.com>
2016-07-24 16:15:59 +0200

    added changed resource file names

--- KnittingApp/app/src/main/java/de/muffinworks/knittingapp/PatternListActivity.java
+++ KnittingApp/app/src/main/java/de/muffinworks/knittingapp/PatternListActivity.java
@@ -1,17 +1,13 @@
 package de.muffinworks.knittingapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-import de.muffinworks.knittingapp.adapters.PatternListAdapter;
-import de.muffinworks.knittingapp.services.models.Metadata;
-import de.muffinworks.knittingapp.util.Constants;
+import de.muffinworks.knittingapp.views.adapters.PatternListAdapter;
 
 /**
  * Created by Bianca on 23.07.2016.
@@ -24,7 +20,7 @@ public class PatternListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pattern_list_activity);
+        setContentView(R.layout.activity_pattern_list);
 
         mPatternsList = (ListView) findViewById(R.id.patterns_list);
         mAdapter = new PatternListAdapter(this);
